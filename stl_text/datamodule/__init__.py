ae23cbe26ac451b9a8245d57c804f7e093cd5514
Jean Maillard <jeanm@fb.com>
2020-12-13 20:18:29 -0800

    Implement concatenative pair classification

--- stl_text/datamodule/__init__.py
+++ stl_text/datamodule/__init__.py
@@ -1,10 +1,11 @@
-from .doc_classification import DocClassificationDataModule
+from .doc_classification import ConcatPairDocClassificationDataModule, DocClassificationDataModule
 from .translation import TranslationDataModule
 
 # As nightly version of torchtext is tricky to setup correctly
 # We removed QuestionAnswerDataModule and TextClassificationDataModule to make dependency to torchtext optional
 
 __ALL__ = [
+    "ConcatPairDocClassificationDataModule",
     "DocClassificationDataModule",
     "TranslationDataModule",
 ]
