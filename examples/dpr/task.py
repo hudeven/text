75e394b34a528315ea6a10491736352236b54a84
Todor Mihaylov <tbmihaylov@fb.com>
2020-12-29 19:09:15 -0800

    Added masking before softmax. Changed evaluation to avg rank.

--- examples/dpr/task.py
+++ examples/dpr/task.py
@@ -49,11 +49,10 @@ class DenseRetrieverTask(LightningModule):
         contexts_repr_flat = self._encode_sequence(contexts_ids_flat, self.context_model) # bs * ctx_cnt x d
         contexts_repr = torch.reshape(contexts_repr_flat, (bs, ctx_cnt, -1)) # bs x ctx_cnt x d
 
-        scores = torch.matmul(query_repr, torch.transpose(contexts_repr, 1, 2)) # bs x ctx_cnt
-        
+        scores = torch.einsum("bd,bld->bl",query_repr, contexts_repr)
         contexts_mask = self._get_mask(contexts_ids)
-        # TO DO: mask the log_softmax
-        softmax_scores = F.log_softmax(scores, 1)
+        
+        softmax_scores = F.log_softmax(self._mask_before_softmax(scores, contexts_mask), 1)
         
         return softmax_scores
 
@@ -67,14 +66,13 @@ class DenseRetrieverTask(LightningModule):
         contexts_is_pos = batch["contexts_is_pos"] # bs x ctx_cnt
         
         pred_context_scores = self(query_ids, contexts_ids)
-        loss = self.loss(pred_context_scores, contexts_is_pos.long())
+        loss = self.loss(pred_context_scores, torch.argmax(contexts_is_pos.long(), dim=1))
         
         return loss
 
     def _get_correct_predictions_cnt(self, pred_scores, correct, mask):
         """
-            Predictions with max score are considered "correct". 
-            
+            Predictions with max score are considered "correct".             
         """
         max_score, max_idxs = torch.max(pred_scores, 1)
         correct_predictions = (max_idxs == torch.tensor(correct).to(max_idxs.device)) * mask
@@ -88,31 +86,45 @@ class DenseRetrieverTask(LightningModule):
         contexts_is_pos = batch["contexts_is_pos"] # bs x ctx_cnt
         
         pred_context_scores = self(query_ids, contexts_ids)
-        loss = self.loss(pred_context_scores, contexts_is_pos.long())
-        
-        #correct_predictions_count = self._get_correct_predictions_cnt(pred_context_scores, contexts_is_pos, self._get_mask(contexts_ids))
-        
         contexts_mask = self._get_mask(contexts_ids)
+
         return pred_context_scores, contexts_is_pos, contexts_mask
 
+    def _mask_before_softmax(self, value, mask):
+        """
+            Mask with very negative numbers.
+        """
+        mask = mask.float()
+        masked_value = value * mask + (mask + 1e-45).log()
+        return masked_value
+        
+
     def _eval_epoch_end(self, outputs):
-        total_acc, total_count = 0, 0
-        for i, (predited_labels, target_labels, mask) in enumerate(outputs):
-            total_acc += ((predited_labels == target_labels) * mask).sum().item() / mask.sum()
-            total_count += predited_labels.size(0)
-        return total_acc, total_count
+        # With this data we dont have the gold rank but many positive and negative contexts. 
+        # So we will count what number of positives are in top N
+
+        total_avg_rank, total_count = 0, 0
+        for i, (pred_scores, target_labels, mask) in enumerate(outputs):
+            values, indices = torch.sort(pred_scores, dim=1, descending=True)
+            avg_rank = torch.sum(indices * target_labels, dim=1)
+            total_avg_rank += avg_rank.sum()
+            total_count += pred_scores.size(0)
+
+        return total_avg_rank, total_count
 
     def validation_step(self, batch, batch_idx):
         return self._eval_step(batch, batch_idx)
 
     def validation_epoch_end(self, valid_outputs):
-        total_acc, total_count = self._eval_epoch_end(valid_outputs)
-        self.log('val_acc', total_acc/total_count, prog_bar=True)
+        total_avg_rank, total_count = self._eval_epoch_end(valid_outputs)
+        self.log('total_avg_rank', total_count, prog_bar=True)
+        self.log('test_avr_rank', total_avg_rank/total_count, prog_bar=True)
 
     def test_step(self, batch, batch_idx):
         return self._eval_step(batch, batch_idx)
 
     def test_epoch_end(self, test_outputs):
-        total_acc, total_count = self._eval_epoch_end(test_outputs)
-        self.log('test_acc', total_acc/total_count, prog_bar=True)
+        total_avg_rank, total_count = self._eval_epoch_end(test_outputs)
+        self.log('total_avg_rank', total_count, prog_bar=True)
+        self.log('test_avr_rank', total_avg_rank/total_count, prog_bar=True)
 
