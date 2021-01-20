35fc0276eb8902894cd4b9f4b8ac7b3226dbb5fe
Parmeet  Singh Bhatia <parmeetbhatia@fb.com>
2021-01-20 12:10:34 -0500

    modified whitespacetokenizer to use torchtext vocab

--- stl_text/ops/tokenizers/whitespace_tokenizer.py
+++ stl_text/ops/tokenizers/whitespace_tokenizer.py
@@ -1,44 +1,43 @@
 import torch.nn as nn
 from typing import Dict, List, Optional
+from collections import OrderedDict
 from iopath.common.file_io import PathManager
-
+from torchtext.experimental.vocab import vocab
 
 class WhitespaceTokenizer(nn.Module):
-    def __init__(self, vocab_path: Optional[str] = None, trainable=False, speed: int = 0):
+    def __init__(self, vocab_path: Optional[str] = None, trainable: bool = False):
         super(WhitespaceTokenizer, self).__init__()
         self.trainable = trainable
-        self.speed = speed  # mock a real tokenizer: slowing down tokenization speed
-
-        self.unknown = "unknown"
-        self.vocab: Dict[str, int] = {self.unknown: 0}
 
         # load vocab
-        path_manager = PathManager()
+        ordered_dict = OrderedDict()
         if vocab_path:
+            path_manager = PathManager()
             with path_manager.open(vocab_path, "r") as f:
                 for line in f.readlines():
                     token = line.split()[0]
-                    self.vocab[token] = len(self.vocab)
+                    ordered_dict[token] = 1
+ 
+        self.vocab = vocab(ordered_dict)
+        self.vocab = self.vocab.to_ivalue()
+
 
     def forward(self, text: str) -> List[int]:
         return self.numberize(self.tokenize(text))
 
     def tokenize(self, text: str) -> List[str]:
-        # slowing down tokenization speed
-        count = 0
-        for i in range(self.speed):
-            count += 1
         return text.split()
 
     def numberize(self, tokens: List[str]) -> List[int]:
-        token_ids: List[int] = []
-        for token in tokens:
-            if token in self.vocab:
-                token_ids.append(self.vocab[token])
-            else:
-                if self.trainable:
-                    token_ids.append(len(self.vocab))
-                    self.vocab[token] = len(self.vocab)
-                else:
-                    token_ids.append(self.vocab[self.unknown])
+        token_ids = self.vocab(tokens)
+
+        if self.trainable:
+            unk_id = self.vocab.__getitem__('<unk>')
+
+            for i,(token,token_id) in enumerate(zip(tokens,token_ids)): 
+                if token_id==unk_id:
+                    self.vocab.append_token(tokens[i])
+                    token_ids[i] = self.vocab.__getitem__(token)
+
         return token_ids
+
