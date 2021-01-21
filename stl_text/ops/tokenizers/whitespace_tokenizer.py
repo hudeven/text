e12f0328f4bc621f2868ff9e93f69bbb5261fac4
Parmeet  Singh Bhatia <parmeetbhatia@fb.com>
2021-01-21 16:14:33 -0500

    using load_vocab_from_file in WhitespaceTokenizer

--- stl_text/ops/tokenizers/whitespace_tokenizer.py
+++ stl_text/ops/tokenizers/whitespace_tokenizer.py
@@ -4,29 +4,21 @@ from typing import Dict, List, Optional
 from collections import OrderedDict
 from iopath.common.file_io import PathManager
 from torchtext.experimental.vocab import (
-    build_vocab_from_text_file,
+    load_vocab_from_file,
     Vocab,
 )
 from torchtext._torchtext import Vocab as VocabPybind 
-
-class SplitTextModule(nn.Module):
-    def __init__(self):
-        super(SplitTextModule,self).__init__()
-
-    def forward(self,x:str):
-        return x.split()
 
 class WhitespaceTokenizer(nn.Module): 
     def __init__(self, vocab_path: Optional[str] = None, trainable: bool = False):
         super(WhitespaceTokenizer, self).__init__()
         self.trainable = trainable
-        self.tokenizer = SplitTextModule()
         self.unk_token = '<unk>'
 
         if vocab_path:
             path_manager = PathManager()
             with path_manager.open(vocab_path, "r",encoding='utf-8') as f:
-                self.vocab = build_vocab_from_text_file(f,torch.jit.script(self.tokenizer))
+                self.vocab = load_vocab_from_file(f,unk_token=self.unk_token)
         else:
             self.vocab = Vocab(VocabPybind([self.unk_token],self.unk_token))
 
@@ -34,9 +26,7 @@ class WhitespaceTokenizer(nn.Module):
 
 
     def forward(self, text: str) -> List[int]:
-        tokens = self.tokenizer(text)
-        return self.numberize(tokens)
-
+        return self.numberize(text.split())
 
     def numberize(self, tokens: List[str]) -> List[int]:
         token_ids = self.vocab(tokens)
@@ -45,7 +35,7 @@ class WhitespaceTokenizer(nn.Module):
 
             for i,(token,token_id) in enumerate(zip(tokens,token_ids)): 
                 if token_id==unk_id:
-                    self.vocab.append_token(tokens[i])
+                    self.vocab.append_token(token)
                     token_ids[i] = self.vocab([token])[0]
 
         return token_ids
